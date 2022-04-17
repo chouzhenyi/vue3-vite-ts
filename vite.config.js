112b623cd74903b338c22e6b6fad54539d577ad5
zhouzhengyi <zhouzhengyi@xuetangx.com>
2022-04-17 23:59:06 +0800

    host

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    '@': path.resolve(__dirname, './src'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    host: '0.0.0.0'
   },
 })
