06c4039b170f4272d3f2db40892f333f10c48554
zhouzhengyi <zhouzhengyi@xuetangx.com>
2022-04-17 23:47:07 +0800

    vit 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Index from './components/Index.vue'
+</script>
+
+<template>
+  <Index msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import { add } from '@/js/utils/tools.js';
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+
+console.log(add(1, 2))
+const title = '赵铁柱'
+const handle = () => {
+  console.log('上班', count.value++);
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="count++">count is: {{ count }}</button>
+  <div @click="handle">烫头 {{ title }}</div>
+</template>
+
+<style scoped>
+</style>
