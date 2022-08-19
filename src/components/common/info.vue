e7b803d26674ccd60f3958a5f541a48efce4bf10
chouzhenyi <zzyatbeijing@163.com>
2022-08-19 15:21:35 +0000

    Deploying to gh-pages from @ chouzhenyi/vue3-vite-ts@50279b3c0ab2f97bc120ac4d9a09a1e91714b9c1 🚀

--- src/components/common/info.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-import { h } from 'vue'
-
-export default {
-  render() {
-    return h('div', {}, '哇哦')
-  }
-}
-</script>
--- src/js/utils/calculate.ts
+++
@@ -1,5 +0,0 @@
-export const add = (list: number[]):number => {
-    return list.reduce((prev, cur) => {
-        return prev + cur;
-    });
-}
