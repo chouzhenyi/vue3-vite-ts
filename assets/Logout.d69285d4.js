42f6a42fe352b2a82e85d5e1a17eea601a9d7d95
chouzhenyi <zzyatbeijing@163.com>
2022-08-19 16:09:21 +0000

    Deploying to gh-pages from @ chouzhenyi/vue3-vite-ts@d62c60213294a6e88da41394465b2ba4fa0f2ec0 🚀

--- assets/Logout.d69285d4.js
+++
@@ -1,1 +0,0 @@
-import{r as e,$ as o,o as r,c as a,t as s}from"./index.a21ec017.js";const p={setup(l){const t=e(o("logout"));return localStorage.removeItem("userInfo"),(c,n)=>(r(),a("h1",null,s(t.value),1))}};export{p as default};
